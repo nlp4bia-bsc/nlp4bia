a1c354c1cccf842fc713ac97a1a8f55573ec9fee
Alberto Becerra <abecerr1@bsc.es>
2024-11-14 22:59:42 +0100

    fix gazetteer columns

--- nlp4bia/datasets/config.py
+++ nlp4bia/datasets/config.py
@@ -2,6 +2,7 @@ import os
 
 ORIG_VERSION = "raw"
 DS_COLUMNS =  ["filenameid", "mention_class", "span", "code", "sem_rel", "is_abbreviation", "is_composite", "needs_context", "extension_esp"]
+GZ_COLUMNS = ["code", "language", "term", "semantic_tag", "mainterm"]
 
 try:
     NLP4BIA_DATA_PATH = os.environ["NLP4BIA_DATA_PATH"]
